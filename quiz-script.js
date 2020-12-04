f4f314012305f9497b1b0f5f96910574751c88ec
gabrieleogoncalves <61953670+gabrieleogoncalves@users.noreply.github.com>
2020-12-03 22:57:24 -0300

    Create quiz-script.js

---
+++ quiz-script.js
@@ -0,0 +1,52 @@
+const ap3 = [
+    {
+        q:'O que é a COVID-19 ?',
+        opcoes:['uma doença infecciosa causada por um vírus.','um resfriado comum como a gripe.','nenhuma das opções anteriores.'],
+        respostas:0
+    },
+    {
+        q:'Como a COVID-19 é transmitida?',
+        opcoes:['através de água poluída.','através de gotículas expelidas pela pessoa com COVID-19 ao tossir ou falar.','nenhuma das opções anteriores.'],
+        respostas:1
+    },
+    {
+        q:'Quais são os sintomais mais comuns da COVID-19?',
+        opcoes:['tosse e cansaço','febre contínua','todas as opções acima.'],
+        respostas:2
+    },
+    {
+        q:'Humanos podem ser contaminados por coronavírus por fonte animal?',
+        opcoes:['sim, através de morcegos','sim, através de animais domésticos','não, não há comprovação científica desse tipo de contágio'],
+        respostas:2
+    },
+    {
+        q:'O que devo fazer se sentir sintomas de COVID-19?',
+        opcoes:['nada','ficar em casa, fazer autoisolamento','nenhuma das opções anteriores.'],
+        respostas:1
+    },
+    {
+        q:'Pessoas sem sintomas podem transmitir o coronavírus?',
+        opcoes:['não, pessoas sem sintomas não tem coronavírus','não, somente as com sintomas','sim, muitas pessoas que possuem coronavírus são assintomáticas'],
+        respostas:2
+    },
+    {
+        q:'Existe alguma cura para o coronavírus?',
+        opcoes:['sim, a cloroquina','não, ainda está sendo realizado estudos para uma possível vacina','sim, ivermectina'],
+        respostas:1
+    },
+    {
+        q:'Como se prevenir do coronavírus?',
+        opcoes:['máscara','lavar as mãos','todas as opções acima'],
+        respostas:2
+    },
+    {
+        q:'Das opções abaixo, qual delas faz parte do grupo de risco da COVID-19?',
+        opcoes:['idosos','crianças','adolescentes'],
+        respostas:0
+    },
+    {
+        q:'Quem mora com pessoas consideradas do grupo de risco também pode ser classificado como do grupo de risco?',
+        opcoes:['não','sim','sim, corre o mesmo risco de pegar o COVID-19'],
+        respostas:0
+    }
+]
